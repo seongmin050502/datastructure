b314d3d3257da5334a890dd8b87942773e1097e6
seongmin050502 <seongmin050502@gmail.com>
2025-04-07 17:46:14 +0900

    Update week05.py

--- week05.py
+++ week05.py
@@ -26,9 +26,16 @@ class Stack:
         return popped_node.data
 
 
+    def peek(self):
+        return self.top.data
+
+
 s1 = Stack()
-print(s1.pop())
+# print(s1.pop())
 s1.push("Data structure")
 s1.push("Database")
-print(s1.pop())
-print(s1.pop())
+# print(s1.pop())
+s1.pop()
+print(s1.peek())
+# for i in range(2):
+#     print(s1.pop())
